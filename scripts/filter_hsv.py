89831e54a160735ba760cf8a9de0c28cd493bad4
Devson Butani <dbutani@ltu.edu>
2024-10-03 02:49:21 -0400

    Added HSV script and renamed methods

--- scripts/filter_hsv.py
+++ scripts/filter_hsv.py
@@ -1,34 +1,49 @@
 #!/usr/bin/env python3
 
+import numpy as np
 import rospy
 from cv_bridge import CvBridge, CvBridgeError
 from dynamic_reconfigure.server import Server
 from sensor_msgs.msg import Image
 
 from ros_cv_tools import cv_tools
-from ros_cv_tools.cfg import GrayscaleBinaryConfig  # type: ignore
+from ros_cv_tools.cfg import HSVConfig  # type: ignore
 
 bridge = CvBridge()
+
+hsv_low = np.array([0, 0, 0])
+hsv_high = np.array([179, 255, 255])
 
 
 # dynamic reconfigure callback
 def dyn_rcfg_cb(config, level):
-    global config_
+    global config_, hsv_low, hsv_high
+
+    # Ensure low values don't exceed high values for HSV
+    if config.h_low > config.h_high:
+        config.h_low = config.h_high
+    if config.s_low > config.s_high:
+        config.s_low = config.s_high
+    if config.v_low > config.v_high:
+        config.v_low = config.v_high
 
     config_ = config
+
+    hsv_low = np.array([config_.h_low, config_.s_low, config_.v_low])
+    hsv_high = np.array([config_.h_high, config_.s_high, config_.v_high])
 
     return config
 
 
 def image_callback(ros_image):
-    """Image callback. ROS image --> CV image --> grayscale --> binary --> BGR --> ROS image"""
+    """Image callback. ROS image --> CV image --> HSV --> binary --> BGR --> ROS image"""
 
     global config_
     try:
         cv_image = bridge.imgmsg_to_cv2(ros_image, "bgr8")
 
         output_image = bridge.cv2_to_imgmsg(
-            cv_tools.gray_to_bgr(cv_tools.gray_to_binary_mask(cv_tools.bgr_to_gray(cv_image), config_.threshold))
+            cv_tools.hsv_to_bgr(cv_tools.hsv_to_binary_mask(cv_tools.bgr_to_hsv(cv_image), hsv_low, hsv_high))
         )
         image_pub.publish(output_image)
     except CvBridgeError as e:
@@ -37,12 +52,12 @@ def image_callback(ros_image):
 
 
 if __name__ == "__main__":
-    rospy.init_node("filter_grayscale_binary", anonymous=True)
+    rospy.init_node("filter_hsv_binary", anonymous=True)
 
     image_pub = rospy.Publisher(rospy.get_param("~output_image_topic"), Image, queue_size=1)
 
     rospy.Subscriber(rospy.get_param("~input_image_topic"), Image, callback=image_callback, queue_size=1)
-    Server(GrayscaleBinaryConfig, dyn_rcfg_cb)
+    Server(HSVConfig, dyn_rcfg_cb)
 
     try:
         rospy.spin()
--- src/ros_cv_tools/cv_tools.py
+++ src/ros_cv_tools/cv_tools.py
@@ -1,19 +1,37 @@
 import cv2
 
 
-def convert_bgr_to_gray(cv_image):
+def bgr_to_gray(cv_image):
     """Convert BGR image to grayscale"""
 
     return cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
 
 
-def convert_gray_to_binary(cv_image, threshold=127):
+def gray_to_bgr(cv_image):
+    """Convert grayscale image to BGR"""
+
+    return cv2.cvtColor(cv_image, cv2.COLOR_GRAY2BGR)
+
+
+def bgr_to_hsv(cv_image):
+    """Convert BGR image to HSV"""
+
+    return cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+
+
+def hsv_to_bgr(cv_image):
+    """Convert HSV image to BGR"""
+
+    return cv2.cvtColor(cv_image, cv2.COLOR_HSV2BGR)
+
+
+def gray_to_binary_mask(cv_image, threshold=127):
     """Convert grayscale image to binary. Still in grayscale range of 0-255"""
 
     return cv2.threshold(cv_image, threshold, 255, cv2.THRESH_BINARY)
 
 
-def convert_gray_to_bgr(cv_image):
-    """Convert grayscale image to BGR"""
+def hsv_to_binary_mask(cv_image, min_array, max_array):
+    """Convert HSV image to binary. Bounds are expected inside an numpy array in HSV format (H: 0-179, S: 0-255, V: 0-255)"""
 
-    return cv2.cvtColor(cv_image, cv2.COLOR_GRAY2BGR)
+    return cv2.inRange(cv_image, min_array, max_array)
