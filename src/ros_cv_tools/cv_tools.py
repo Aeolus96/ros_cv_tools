ed0375984fdcbb090bc8e33258fd76c38fe578a9
Devson Butani <dbutani@ltu.edu>
2024-10-27 16:48:45 -0400

    bugfix

--- src/ros_cv_tools/cv_tools.py
+++ src/ros_cv_tools/cv_tools.py
@@ -6,6 +6,6 @@ def white_percent(cv_image):
     """Calculate the percentage of white pixels in a black-and-white image"""
 
     white_pixels = np.sum(cv_image == 255)
-    total_pixels = cv_image.size
+    total_pixels = cv_image.shape[0] * cv_image.shape[1]
     percentage = (white_pixels / total_pixels) * 100
     return round(percentage, 2)  # Return rounded percentage
